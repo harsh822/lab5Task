6d09b8df564bdf0c1d2565cb28fca8e34ee8532e
harsh822 <harsh.gautam_cs19@gla.ac.in>
2020-07-20 12:06:59 +0530

    init commit

---
+++ src/lab/labTask1.java
@@ -0,0 +1,9 @@
+
+package lab;
+
+//Task 1 - Create a "Hello, World!" program in Java.
+public class labTask1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/lab/labTask2.java
@@ -0,0 +1,29 @@
+/*
+
+Authors Name= Harsh Gautam
+Date:-20-07-2020
+*/
+package lab;
+
+//Task 2 - Create a program in Java to declare, initialize and use local variables of all the primitive data types in Java.
+public class labTask2 {
+    public static void main(String[] args) {
+        byte Byte = 100;
+        short Short = 523;
+        int Integer = 2333;
+        long Long = 981523652;
+        float Float = 1.55f;
+        boolean Boolean = true;
+        char Character = 'H';
+        double Double = 526.23254521;
+        System.out.println("byte example:" + Byte);
+        System.out.println("short example:" + Short);
+        System.out.println("int example:" + Integer);
+        System.out.println("long example:" + Long);
+        System.out.println("float example:" + Float);
+        System.out.println("boolean example:" + Boolean);
+        System.out.println("char example:" + Character);
+        System.out.println("double example:" + Double);
+    }
+
+}
